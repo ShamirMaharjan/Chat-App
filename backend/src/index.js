244a1bc3c5d7a91c98460af29563b3130d316cdf
Shamir Maharjan <shamirmaharjan07@gmail.com>
2024-12-26 11:23:16 +0545

    added cros

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connect_db } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cros from 'cors';
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,12 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+app.use(cros({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+
 
 app.use("/api/auth", authRoutes);
 
